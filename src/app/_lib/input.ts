e07c5b202c553be4e7030f92c1dd1c92c619ae2d
songha-5 <shn0518@gmail.com>
2026-06-08 14:51:19 +0900

    feat: zod설정 (login, signup)

---
+++ src/app/_lib/input.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod'
+
+const PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9]).{4,}$/
+
+// 스키마
+const emailSchma = z.email('유효한 이메일이 아닙니다.')
+const passwordSchma = z
+  .string('유효한 비밀번호가 아닙니다.')
+  .regex(PASSWORD_REGEX, "영어, 숫자 포함 4자리 이상이여야 합니다.")
+  .min(4, { message: '비밀번호는 4자 이상이여야 합니다.'})
+const confirmPasswordSchma = z.string()
+const nameSchema = z.string().min(2, '닉네임은 2자 이상이여야 합니다.')
+  
+// 로그인 유효성 검사
+export const LoginSchma = z.object({
+  email: emailSchma,
+  password: passwordSchma
+})
+
+// 회원가입 유효성 검사
+export const SignupSchma = z.object({
+  email: emailSchma,
+  password: passwordSchma,
+  confirmPassword: confirmPasswordSchma,
+  name: nameSchema
+}).refine(data => data.password === data.confirmPassword, {
+  path: ['confirmPassword'],
+  message: '비밀번호가 일치하지 않습니다.',
+})
+
+
+export type LoginFormData = z.infer<typeof LoginSchma>
+export type SignupFormData = z.infer<typeof SignupSchma>
