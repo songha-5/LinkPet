e282071ee7371cbe3d95a65e69bd5c8fab533981
songha-5 <shn0518@gmail.com>
2026-06-12 10:58:27 +0900

    feat: Header / Footer

---
+++ src/app/_components/header/Header.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="border-b-4 border-neonPink static">
+      <div className="flex items-center justify-between max-w-7xl m-auto text-neonGreen py-3 px-6">
+        <Link href={"/user"} className="text-[32px]">[ ^•ﻌ•^ ] LINKPET</Link>
+        <span>SECURE_CORE_NODE_V3</span>
+      </div>
+    </header>
+  )
+}
