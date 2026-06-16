7284a16777992f9c41bdf50740dbae12a8ca4a30
songha-5 <shn0518@gmail.com>
2026-06-16 17:40:05 +0900

    feat: 비밀번호 변경( 유저가 로그인 되어있을 시)

--- src/app/_lib/auth.ts
+++ src/app/_lib/auth.ts
@@ -52,9 +52,20 @@ export const NameChangeSchma = z.object({
   name: nameSchema
 })
 
+// (로그인 후) 비밀번호 변경
+export const PasswordUserChangeSchma = z.object({
+  userPassword: passwordSchma,
+  password: passwordSchma,
+  confirmPassword: confirmPasswordSchma
+}).refine(data => data.password === data.confirmPassword, {
+  path: ['confirmPassword'],
+  message: '새 비밀번호가 일치하지 않습니다.',
+})
+
 export type LoginFormData = z.infer<typeof LoginSchma>
 export type SignupFormData = z.infer<typeof SignupSchma>
 export type PasswordFormData = z.infer<typeof PasswordSchma>
 export type PasswordEmailFormData = z.infer<typeof PasswordEmailSchma>
 export type PasswordChangeFormData = z.infer<typeof PasswordChangeSchma>
 export type NameChangeFormData = z.infer<typeof NameChangeSchma>
+export type PasswordUserChangeFormData = z.infer<typeof PasswordUserChangeSchma>
--- src/app/user/_components/UserModal.tsx
+++ src/app/user/_components/UserModal.tsx
@@ -7,12 +7,11 @@ import NameChangeModal from "../../_components/modal/NameChangModal";
 
 export default function UserModal() {
   const openModal = useModalStore((state) => state.openModal)
-  const closeModal = useModalStore((state) => state.closeModal)
 
   return (
     <div className="mt-10">
       <BaseButton onClick={() => openModal(<NameChangeModal />)} content="닉네임 변경 (UPDATE_NAME)" outline color="white" className="text-[18px] h-14!"/>
-      <BaseButton onClick={() => openModal(<PasswordModal onClick={closeModal} />)} content="비밀번호 변경 (PATCH_PW)" outline color="white" className="text-[18px] h-14! mt-4"/>
+      <BaseButton onClick={() => openModal(<PasswordModal />)} content="비밀번호 변경 (PATCH_PW)" outline color="white" className="text-[18px] h-14! mt-4"/>
     </div>
   )
 }
