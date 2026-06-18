4dee7c005c6d31d4cfa83ad8ee72cd65d932ee80
songha-5 <shn0518@gmail.com>
2026-06-18 16:02:54 +0900

    style: footer가 잘리는 현상 수정

--- src/app/qna/[id]/[page]/layout.tsx
+++ src/app/qna/[id]/[page]/layout.tsx
@@ -5,7 +5,7 @@ export default function QnALayout({ children }: React.PropsWithChildren ) {
   return (
     <>
       <Header />
-      <main className="w-full h-full relative pbs-25 px-6 mbe-10 max-w-7xl m-auto">
+      <main className="w-full mt-0 relative pbs-25 px-6 mbe-10 max-w-7xl m-auto">
         {children}
       </main>
       <Footer />
