05745585b08138b15cebfd313026b4a1599e282c
songha-5 <shn0518@gmail.com>
2026-06-18 15:45:54 +0900

    feat: 반응형 추가(일정이상 안늘어나게 변경)

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="border-b-4 border-neonPink fixed z-1 w-full bg-bg">
-      <div className="flex items-center justify-between text-neonGreen py-3 px-6">
+      <div className="flex items-center justify-between text-neonGreen py-3 px-6 max-w-7xl m-auto">
         <Link href={"/user"} className="text-[32px]" aria-label="LinkPet">[ ^•ﻌ•^ ] LINKPET</Link>
         <span>SECURE_CORE_NODE_V3</span>
       </div>
