f7c5564821fd3b0afaeed0a40317a44133513b62
songha-5 <shn0518@gmail.com>
2026-06-23 14:33:58 +0900

    feat: 글 등록 유효성 추가

--- src/app/_lib/qna.ts
+++ src/app/_lib/qna.ts
@@ -2,6 +2,7 @@ import z from "zod"
 
 const questTitleSchema = z.string().min(2, '질문 제목은 2자 이상이여야 합니다.')
 const questContentSchema = z.string().min(18, '질문은 10자 이상이여야 합니다.')
+const questContentAdminSchema = z.string().min(18, '답변은 10자 이상이여야 합니다.')
 
 // 게시글 등록 유효성 검사
 export const QnACreateSchema = z.object({
@@ -12,6 +13,11 @@ export const QnACreateSchema = z.object({
 export const TextareaSchema = z.object({
   content: questContentSchema
 })
+// 어드민 - 게시글 등록 유효성 검사
+export const QnACreateAdminSchema = z.object({
+  content: questContentAdminSchema
+})
 
 export type QnACreateFormData = z.infer<typeof QnACreateSchema>
 export type TextareaFormData = z.infer<typeof TextareaSchema>
+export type QnACreateAdminFormData = z.infer<typeof QnACreateAdminSchema>
