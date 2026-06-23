246097f47ad722c7822b5047a5c1b30fbcdb807b
songha-5 <shn0518@gmail.com>
2026-06-23 13:50:07 +0900

    feat: 유저가 맞는지 확인하는 함수 변경 - 기능에 맞는 명칭으로 변경 - user가 없을때(로그아웃 등)와 에러발생(강제성)때를 분기

--- src/app/_lib/userChecked.ts
+++ src/app/_lib/userChecked.ts
@@ -1,13 +1,19 @@
 import { createClient } from "@/src/utils/supabase/server"
+import { redirect } from "next/navigation"
 
-export const getUser = async () => {
+export const userChecked = async () => {
   const supabase = await createClient()
   const { data: { user }, error: authError } = await supabase.auth.getUser()
 
-  if (!user || authError) {
+  if (!user) {
+    console.log("세션이 만료됬습니다.")
+    redirect('/')
+  }
+  
+  if (authError) {
     console.log("세션이 만료됬습니다.")
     throw new Error("로그인이 필요합니다.")
   }
 
   return user
-}
+} 
