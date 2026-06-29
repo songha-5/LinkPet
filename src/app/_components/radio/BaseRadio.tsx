8df9bfad12260eecc2d2aed12952a0a6c7e68bd6
songha-5 <shn0518@gmail.com>
2026-06-29 16:20:36 +0900

    feat: 라디오 컴포넌트화

--- src/app/_components/radio/BaseRadio.tsx
+++ src/app/_components/radio/BaseRadio.tsx
@@ -13,7 +13,7 @@ export default function BaseRadio({ name, value, content, className }: BaseRadio
   return (
     <div className={`flex-1 ${className}`}>
       <input className="peer sr-only" id={id} type="radio" name={name} value={value} />
-      <label className="flex peer-checked:[&_span]:inline-block justify-center cursor-pointer items-center border-5 shadow-[4px_4px_0_var(--color-font-white-shadow)] border-font-white text-lg h-14 bg-bg hover:border-neonPink hover:text-neonPink hover:shadow-[4px_4px_0_var(--color-neonPink)] transition-all peer-checked:text-neonPink peer-checked:border-neonPink peer-checked:shadow-[4px_4px_0_var(--color-neonPink)]" htmlFor={id} >
+      <label className="flex peer-checked:[&_span]:inline-block justify-center cursor-pointer items-center border-5 shadow-[4px_4px_0_var(--color-font-white-shadow)] border-font-white text-lg h-14 bg-bg hover:border-neonPink hover:text-neonPink hover:shadow-[4px_4px_0_var(--color-neonPink)] transition-all peer-checked:text-neonGreen peer-checked:border-neonGreen peer-checked:shadow-[4px_4px_0_var(--color-neonGreen)] peer-checked:hover:border-neonGreen peer-checked:hover:text-neonGreen peer-checked:hover:shadow-[4px_4px_0_var(--color-neonGreen)]" htmlFor={id} >
         <span className="hidden me-1 animate-opacityBlink">▶</span>
         {content}
       </label>
