8a34217511c8c28b9f41f1b969068492bcf82378
songha-5 <shn0518@gmail.com>
2026-07-02 10:21:12 +0900

    feat: 파일 추가시 미리보기 이미지파일 변경

--- src/app/_components/button/ImageButton.tsx
+++ src/app/_components/button/ImageButton.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 interface ImageButtonProps {
   className?: string
@@ -7,6 +7,7 @@ interface ImageButtonProps {
 }
 
 export default function ImageButton({ className, onFileSelect }: ImageButtonProps) {
+  const [fileName, setFileName] = useState<string | null>(null)
   const fileInputRef = useRef<HTMLInputElement>(null)
 
   // input이 대신 실행
@@ -17,10 +18,16 @@ export default function ImageButton({ className, onFileSelect }: ImageButtonProp
   // 파일이 선택되었을때 실행
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0]
+    // 유저 프로필
+    const newAvata = file ? URL.createObjectURL(file) : null
+    const profile = newAvata || './bg_2.svg'
+
     if(file && onFileSelect) {
       onFileSelect(file)
+      setFileName(profile)
     }
 
+    console.log(typeof file?.name)
     if (e.target) e.target.value = ""
   }
 
@@ -34,7 +41,7 @@ export default function ImageButton({ className, onFileSelect }: ImageButtonProp
         onChange={handleFileChange}
       />
       <button type="button" onClick={handleClick} className="peer cursor-pointer border-4 border-dashed bg-bg relative w-50 h-50 border-neonPink">
-        <Image src="./bg_2.svg" fill alt="" className="object-cover" />
+        <Image src={fileName || `./bg_2.svg`} fill alt="" className="object-cover" />
 
         <div className="absolute -right-1 -bottom-1 w-10 h-10 border-4 border-neonPink bg-neonPink">
           <svg viewBox="0 0 11 11" width="20" height="20" className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 fill-font-white">
